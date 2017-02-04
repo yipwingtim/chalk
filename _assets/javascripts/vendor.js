51aa7183c4086ebd62a4d6bb40b160cb6ba8a4f1
Nielsen Ramon <hello@nielsenramon.com>
2017-02-04 11:55:23 +0100

    nr-picture-logo | wip

--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,5 +1,5 @@
 //= require jquery
 //= require vendor/jquery-throttle-debounce.js
 //= require fluidbox
+//= require retina.js/dist/retina.js
 //= require scrollreveal
-//= require vendor/retina.js
